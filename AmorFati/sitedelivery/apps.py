58629d6a74d0eb55cabbbaf930ac5a39cb0b2a71
Armagedom100 <alvinholeitao@gmail.com>
2024-05-29 13:58:23 -0300

    tentando montar
    close 3#

---
+++ AmorFati/sitedelivery/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SitedeliveryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AmorFati.sitedelivery'
--- AmorFati/urls.py
+++ AmorFati/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from AmorFati.home_view import home
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', home,),
+    path('sitedelivery/', include('AmorFati.sitedelivery.urls')),
 ]
